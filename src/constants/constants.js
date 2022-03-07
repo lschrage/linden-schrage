716b08b31beec26d7cdfb8713d41a9ce28f5d86f
Linden Schrage <lschrage@college.harvard.edu>
2022-03-07 17:01:19 -0500

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: 'Rocket Launch App',
+    description: "Gimme Space is a website that allows users to sign up for automatic notifications about upcoming rocket launches.",
+    image: '/images/gimmespace.png',
+    visit: 'http://gimme-space.herokuapp.com/',
+    title_content: 'Website',
+    id: 0,
+  },
+  {
+    title: 'Metagenomic Analysis',
+    description: "Performed metagenomic statistical analysis of data from Alaskan permafrost microbial communities.",
+    image: '/images/metagenomic.png',
+    visit: '/files/abstract_metagenomic_analysis.pdf',
+    title_content: 'Data Analysis',
+    id: 1,
+  },
+  {
+    title: 'CNN Doplphin Fin Classification',
+    description: "Conducted a study to investigate the aerosol removal efficacy of HEPA purifiers in thirteen different rooms on the Harvard College campus.",
+    image: '/images/cnn.png',
+    visit: 'https://colab.research.google.com/drive/1Kafaw3QUpMfZ4iJumf8N42w0uIdA3K1J?usp=sharing',
+    title_content: 'Data Analysis',
+    id: 2,
+  },
+  {
+    title: 'Covid Task Force Report',
+    description: "Conducted a study to investigate the aerosol removal efficacy of HEPA purifiers in thirteen different rooms on the Harvard College campus.",
+    image: '/images/data.png',
+    visit: '/files/coviddata.pdf',
+    title_content: 'Data Analysis',
+    id: 2,
+  },
+  {
+    title: "Metamodern Analysis of Bo Burnham's Inside",
+    description: "Conducted a study to investigate the aerosol removal efficacy of HEPA purifiers in thirteen different rooms on the Harvard College campus.",
+    image: '/images/metamodern.png',
+    visit: '/files/metamodern_essay.pdf',
+    title_content: 'Writing Sample',
+    id: 3,
+  },
+];
+
